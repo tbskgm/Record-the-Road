afd87a7c77dd3d013d18ee551299fb658f3fde2c
tbakgm <yxiaoju01@gmail.com>
2020-12-03 17:25:06 +0900

    [add]

---
+++ Classes.swift
@@ -0,0 +1,9 @@
+//
+//  Classes.swift
+//  Record the Road
+//
+//  Created by 小駒翼 on 2020/08/19.
+//  Copyright © 2020 Tsubasa Kogoma. All rights reserved.
+//
+
+import Foundation
---
+++ Protocols.swift
@@ -0,0 +1,9 @@
+//
+//  Protocols.swift
+//  Record the Road
+//
+//  Created by 小駒翼 on 2020/08/20.
+//  Copyright © 2020 Tsubasa Kogoma. All rights reserved.
+//
+
+import Foundation
