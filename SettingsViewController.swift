9acda6350c7ff16ef7995e04e44b3c46dc8e0faf
tbakgm <yxiaoju01@gmail.com>
2020-12-03 17:25:57 +0900

    [add]

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -11,53 +11,23 @@ import EventKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let recordOnyourCalendar = RecordOnYourCalendar()
-        
-        //カレンダーへのアクセス許可を取る
-        guard recordOnyourCalendar.requestAuthorization() == true else {
-            //アラートを表示
-            //ボタンを押せないようにする処理
-            //戻る処理(dismissの事)
-            return
-        }
+        inputTextView()
+        //button.isEnabled = false
         
     }
     
     
+    //無駄なログを削除する
     @IBAction func button(_ sender: Any) {
-        
-        func recordCalendar() {
-            let userDefaults: UserDefaults = UserDefaults.standard
-            
-            guard let array = userDefaults.object(forKey: "collectedInfomation") as? [[String: Any]] else {
-                return
-            }
-            guard let collectedInfomation = array.last else {
-                return
-            }
-            let latitude = collectedInfomation["latitude"] as! Double
-            let longitude = collectedInfomation["longitude"] as! Double
-            let timestamp = collectedInfomation["timestamp"] as! Date
-            
-            var notes: String {
-                "経度:\(longitude)\n緯度: \(latitude)\n時間: \(timestamp)"
-            }
-            
-            //カレンダーに記録する
-            let recordOnYourCalendar: RecordOnYourCalendar = RecordOnYourCalendar()
-            recordOnYourCalendar.recordEvent(title: "行動履歴", viewController: self, notes: notes)
-            
-            //戻る処理
-            let queue: DispatchQueue = DispatchQueue.main
-            queue.asyncAfter(deadline: .now() + 1) {
-                self.dismiss(animated: true, completion: nil)
-            }
-        }
-        
-        recordCalendar()
+        //save()
+        button.isEnabled = false
     }
     
     
@@ -66,88 +36,98 @@ class SettingsViewController: UIViewController {
     }
     
     
-}
-
-
-
-//カレンダー連携
-class RecordOnYourCalendar {
-    // EventStoreを初期化
-    let eventStore: EKEventStore = EKEventStore()
-    
-    //認証状況を確認して、カレンダーへのアクセスを許可されてなかった場合は許可を得る
-    func requestAuthorization() -> Bool{
-        if getAuthorizationStatus() {
-            print("許可を得ています")
-            return true
-            
-        } else {
-            eventStore.requestAccess(to: .event, completion: {(granted, error) in //https://developer.apple.com/documentation/eventkit/ekeventstore/1507547-requestaccess
-                if granted {
-                    return
-                } else {
-                    print("許可を得ていません")
-                }
-            })
-            return false
-        }
-    }
-    
-    //認証ステータスを確認する
-    func getAuthorizationStatus() -> Bool {
-        //承認ステータスを取得
-        let status = EKEventStore.authorizationStatus(for: .event) //https://developer.apple.com/documentation/eventkit/ekeventstore/1507239-authorizationstatus
-        
-        // ステータスを表示 許可されている場合のみtrueを返す
-        switch status {
-        case .notDetermined:
-            print("NotDetermined")
-            return false
-        case .denied:
-            print("Denied")
-            return false
-        case .authorized:
-            print("Aythorized")
-            return true
-        case .restricted:
-            print("Restricted")
-            return false
-        default:
-            assertionFailure("今までにない新しいケースです。Apple Developerを確認！！") //列挙型の変更に対するお備え
-            return false
-        }
-    }
-    
-    
-    //カレンダーに記録
-    func recordEvent(title: String, viewController view: UIViewController, notes: String) {
+    func inputTextView() {
         /*
-        let authenticationResult: Bool = requestAuthorization() //カレンダーへのアクセス許可を取る
-        guard authenticationResult == true else { //カレンダーへのアクセスが許可されていないときの処理
-            let alert: Alert = Alert()
-            alert.showAlert(viewController: view, message: "カレンダーへのアクセスが許可されていません")
-            return print("カレンダーへのアクセスが許可されていないので、記録できませんでした")
-        }*/
-        // イベントの情報を準備
-        let startDate: Date = Date() //カレンダーの時間指定のために必須
-        let endDate: Date = Date()
-        let defaultCalendar = eventStore.defaultCalendarForNewEvents
-        // イベントを作成して情報をセット
-        let event = EKEvent(eventStore: eventStore)
-        event.title = title
-        event.startDate = startDate
-        event.endDate = endDate
-        event.calendar = defaultCalendar
-        event.notes = notes
-        
-        // イベントの登録
-        do {
-            try eventStore.save(event, span: .thisEvent)
-            print("できました")
-        } catch {
-            print(error)
-            print("できませんでした")
+        let userDefaults: UserDefaults = UserDefaults.standard
+        guard let storedCollectedInfomations = userDefaults.object(forKey: "collectedInfomation") as? [[String: Any]] else {
+            return print("collectedInfomationの値が存在しませんでした")
         }
+        let storedCollectedInfomationsCount = storedCollectedInfomations.count
+        
+        var count = 0
+        var texts: String = ""
+        
+        for storedCollectedInfomation in storedCollectedInfomations {
+            //経度、緯度の取得
+            let latitude = storedCollectedInfomation["latitude"] as! CLLocationDegrees //経度の取得
+            let longitude = storedCollectedInfomation["longitude"] as! CLLocationDegrees //緯度の取得
+            
+            //タイトルの取得
+            /*
+             var title: String!
+             if let date = storedCollectedInfomation["timestamp"] as? Date {
+             let timeRelationship = TimeRelationship()
+             title = timeRelationship.dateToString(date: date) //String型に変換
+             } else {
+             print("titleがありません")
+             //title = ""
+             continue
+             }*/
+            guard let date = storedCollectedInfomation["timestamp"] as? Date else {
+                print("titleがありません")
+                continue
+            }
+            let timeRelationship = TimeRelationship()
+            let title = timeRelationship.dateToString(date: date)
+            print("@x.title: \(String(describing: title))")
+            
+            //サブタイトルの取得
+            guard storedCollectedInfomationsCount > 2 else { //配列の数が足らない時
+                print("storedCollectedInfomationの数が足りません")
+                var locationInfomation: String { "\n経度: \(longitude), 緯度: \(latitude), 時間: \(title)" }
+                return textView.text = locationInfomation
+                
+            }
+            //到着時間の取得
+            let arrival = storedCollectedInfomations[count]
+            let arrivalTime = arrival["timestamp"] as! Date
+            //出発時間の取得
+            count += 1
+            print("count: \(count)")
+            guard (storedCollectedInfomationsCount - 1) != count else { //すべての配列を記録した後に配列外を参照しないため
+                //print("全ての滞在時間を算出しました")
+                //textView.text = texts
+                
+                //continue
+                break
+            }
+            let departure = storedCollectedInfomations[count]
+            let departureTime = departure["timestamp"] as! Date
+            //滞在時間の取得
+            var stayTime: Int { Int(departureTime.timeIntervalSince(arrivalTime)) } //出発時間から到着時間を引く
+            if stayTime < 600 { //滞在時間が指定時間以下だったら次の処理に移行する
+                continue
+            }
+            var subTitle: String { "\(stayTime/3600): \(stayTime%3600/60): \(stayTime%3600%60)" } //テキストに直す
+            //var subTitle: String {text}
+            var locationInfomation: String {"\n経度: \(longitude), 緯度: \(latitude), 時間: \(title), 滞在時間: \(subTitle)"}
+            texts += locationInfomation
+            print("texts: \(texts)")
+        }
+        print("全ての滞在時間を算出しました")
+        textView.text = texts
+        */
+        let aboutLocation = AboutLocation()
+        guard aboutLocation.getLocationData() != nil else {
+            return
+        }
+        let array = aboutLocation.getLocationData()!
+        var texts: String = ""
+        print("class.stayTime: 終了")
+        for i in array {
+            print("class.i: \(i)")
+            var longitude: CLLocationDegrees { i["longitude"] as! CLLocationDegrees } //経度
+            var latitude: CLLocationDegrees { i["latitude"] as! CLLocationDegrees } //緯度
+            var timestamp: Date { i["timestamp"] as! Date } //到着時間
+            var stayTime: Int { i["stayTime"] as! Int } //滞在時間
+            var text : String { "\n経度: \(longitude), 緯度: \(latitude), 時間: \(timestamp), 滞在時間: \(stayTime)" }
+            print("stayTime: \(stayTime)")
+            texts += text
+        }
+        textView.text = texts
     }
     
 }
+
+
+
